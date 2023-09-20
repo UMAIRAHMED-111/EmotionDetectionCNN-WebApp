e837f30b5afb548854f956d1a36160b0751da9bb
Umair Ahmed <109911871+UMAIRAHMED-111@users.noreply.github.com>
2023-09-20 22:47:47 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, jsonify, request
 import cv2
 import numpy as np
 from keras.models import model_from_json
 import os
+import base64
+import io
 
 app = Flask(__name__)
 
@@ -23,23 +25,10 @@ emotion_labels = ['neutral', 'happiness', 'surprise', 'sadness', 'anger', 'disgu
 # Load the Haar Cascade for face detection
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# Function to predict emotion from facial image
 def predict_emotion(face_image):
-    """
-    Predicts emotion from the facial image.
-
-    Parameters:
-        face_image (numpy.ndarray): Grayscale facial image of shape (48, 48).
-
-    Returns:
-        str: Predicted emotion label.
-    """
-    # Resize the face image to match the model input size
     face_image = cv2.resize(face_image, (48, 48))
     face_image = face_image.astype("float") / 255.0
     face_image = np.expand_dims(face_image, axis=0)
-    
-    # Make prediction using the loaded model
     prediction = loaded_model.predict(face_image)
     max_index = np.argmax(prediction)
     emotion = emotion_labels[max_index]
@@ -49,26 +38,37 @@ def predict_emotion(face_image):
 def index():
     return render_template('index.html')
 
+@app.route('/process_frame', methods=['POST'])
+def process_frame():
+    data = request.json
+    encoded_image = data['image'].split(",")[1]
+    decoded_image = base64.b64decode(encoded_image)
+    image_buffer = io.BytesIO(decoded_image)
+    image_buffer.seek(0)
+    file_bytes = np.asarray(bytearray(image_buffer.read()), dtype=np.uint8)
+    frame = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    
+    # Process frame
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
+    emotion = "unknown"
+    for (x, y, w, h) in faces:
+        face_roi = gray[y:y+h, x:x+w]
+        emotion = predict_emotion(face_roi)
+        break  # assuming only one face; adjust if needed
+
+    return jsonify({"emotion": emotion})
+
 def gen():
     """Generate frame for video streaming."""
     camera = cv2.VideoCapture(0)
     while True:
         ret, frame = camera.read()
-
-        # Convert the frame to grayscale for face detection
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        # Detect faces in the frame using Haar Cascade
         faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
-
         for (x, y, w, h) in faces:
-            # Crop the face region for emotion prediction
             face_roi = gray[y:y + h, x:x + w]
-
-            # Predict emotion from the cropped face region
             emotion = predict_emotion(face_roi)
-
-            # Display the predicted emotion on the frame
             cv2.putText(frame, emotion, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 1, cv2.LINE_AA)
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 1)
 
